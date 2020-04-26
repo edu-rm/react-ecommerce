729918495fd097bcaf48d8b640ede1fb4d27b564
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-26 19:06:45 -0300

    redux saga configuration finished

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './modules/rootReducer';
 
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
 const enhancer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware))
+    : applyMiddleware(sagaMiddleware);
 const store = createStore(reducers, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
