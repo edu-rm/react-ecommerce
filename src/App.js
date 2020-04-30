e8bf9eadcde28234de578d20e436abb4393b7328
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-30 16:28:58 -0300

    react toastify: stock error message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
@@ -16,6 +18,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={4000} />
       </BrowserRouter>
     </Provider>
   );
